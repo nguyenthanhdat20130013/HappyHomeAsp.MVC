81aab407305e34fa0429c3424c23eb21dcc16afa
nguyenthanhdat20130013 <20130013@st.hcmuaf.edu.vn>
2022-12-22 14:08:03 +0700

    fix

--- HappyHomeAsp.MVC/Controllers/DiningRoomController.cs
+++ HappyHomeAsp.MVC/Controllers/DiningRoomController.cs
@@ -17,31 +17,9 @@ namespace HappyHomeAsp.MVC.Controllers
         // GET: DiningRoom
         public ActionResult Index()
         {
-            List<Product> products = new List<Product>();
-            string constr = ConfigurationManager.ConnectionStrings["ConString"].ConnectionString;
-            using (MySqlConnection con = new MySqlConnection(constr))
-            {
-                string query = "SELECT * FROM product";
-                using (MySqlCommand cmd = new MySqlCommand(query))
-                {
-                    cmd.Connection = con;
-                    con.Open();
-                    using (MySqlDataReader sdr = cmd.ExecuteReader())
-                    {
-                        while (sdr.Read())
-                        {
-                            Product pro = new Product(sdr.GetInt32(0), sdr.GetString(1),
-                                sdr.GetInt32(2), sdr.GetInt32(3), sdr.GetString(4), sdr.GetString(5),
-                                sdr.GetString(6), sdr.GetString(7), sdr.GetString(8), sdr.GetString(9),
-                                sdr.GetInt32(10), sdr.GetString(11), sdr.GetString(12));
-                            products.Add(pro);
-
-                             
-                        }
-                    }
-                    con.Close();
-                }
-            }
+            ManageData manage = new ManageData();
+            ArrayList products = new ArrayList();
+            products = manage.selectAllProduct2();
 
             return View(products);
         }
--- HappyHomeAsp.MVC/Models/ManageData.cs
+++ HappyHomeAsp.MVC/Models/ManageData.cs
@@ -3,6 +3,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data.Common;
 using System.Linq;
 
@@ -90,6 +91,36 @@ namespace HappyHomeAsp.MVC.Models
 
             return listImage;
 
+        }
+
+        public ArrayList selectAllProduct2()
+        {
+            ArrayList products = new ArrayList();
+            string constr = ConfigurationManager.ConnectionStrings["ConString"].ConnectionString;
+            using (MySqlConnection con = new MySqlConnection(constr))
+            {
+                string query = "SELECT * FROM product";
+                using (MySqlCommand cmd = new MySqlCommand(query))
+                {
+                    cmd.Connection = con;
+                    con.Open();
+                    using (MySqlDataReader sdr = cmd.ExecuteReader())
+                    {
+                        while (sdr.Read())
+                        {
+                            Product pro = new Product(sdr.GetInt32(0), sdr.GetString(1),
+                                sdr.GetInt32(2), sdr.GetInt32(3), sdr.GetString(4), sdr.GetString(5),
+                                sdr.GetString(6), sdr.GetString(7), sdr.GetString(8), sdr.GetString(9),
+                                sdr.GetInt32(10), sdr.GetString(11), sdr.GetString(12));
+                            products.Add(pro);
+
+
+                        }
+                    }
+                    con.Close();
+                    return products;
+                }
+            }
         }
     }
 }
