cac729273fb2439a5ed0ed269af71071b7fb0001
20130236 <115078773+20130236@users.noreply.github.com>
2022-12-25 19:21:07 +0700

    Update DataUserController.cs
    List User

--- HappyHomeAsp.MVC/Areas/Admin/Controllers/DataUserController.cs
+++ HappyHomeAsp.MVC/Areas/Admin/Controllers/DataUserController.cs
@@ -1,17 +1,80 @@
-﻿using System;
+using HappyHomeMVC.DataBase;
+using HappyHomeMVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace HappyHomeAsp.MVC.Areas.Admin.Controllers
+namespace HappyHomeAsp.MVC.Controllers
 {
     public class DataUserController : Controller
     {
         // GET: Admin/DataUser
         public ActionResult Index()
         {
-            return View();
+            List<User> users = UserDAO.findAll();
+            return View(users);
         }
+
+        public ActionResult EditUser(int id)
+        {
+            User user = UserDAO.findUserById(id);
+            return View(user);
+        }
+
+        [HttpPost]
+        public ActionResult EditUser(int id, User user)
+        {
+            String username = user.UserName;
+            String password = user.PassWord;
+            if (string.IsNullOrEmpty(username))
+            {
+                ModelState.AddModelError("errorUserName", "Phải Nhập Tên Đăng Nhập!");
+                return View(user);
+            }
+            else if (string.IsNullOrEmpty(password))
+            {
+                ModelState.AddModelError("errorPass", "Phải Nhập Mật Khẩu!");
+                return View(user);
+            }
+            else
+            {
+                UserDAO.updateUserAdmin(id, user.UserName, user.PassWord, user.Role);
+               return RedirectToAction("Index");
+            }
+        }
+            public ActionResult AddUser()
+            {
+                return View();
+            }
+
+            [HttpPost]
+            public ActionResult AddUser(User user)
+            {
+                String username = user.UserName;
+                String password = user.PassWord;
+                if (string.IsNullOrEmpty(username))
+                {
+                    ModelState.AddModelError("errorUserName", "Phải Nhập Tên Đăng Nhập!");
+                    return View(user);
+                }
+                else if (string.IsNullOrEmpty(password))
+                {
+                    ModelState.AddModelError("errorPass", "Phải Nhập Mật Khẩu!");
+                    return View(user);
+                }
+                else
+                {
+                    UserDAO.addUserAdmin(user);
+                    return RedirectToAction("Index");
+                }
+            }
+
+            public ActionResult DeleteUser(int id)
+            {
+                UserDAO.deleteUserAdmin(id);
+                return RedirectToAction("Index");
+            }
     }
 }
