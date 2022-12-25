d0907d1632586b0b3200aaa4242ee28bc1e87a44
Nguyen-Van-Truong <115326716+Nguyen-Van-Truong@users.noreply.github.com>
2022-12-25 21:28:39 +0700

    phân trang với type là -1 :))

--- HappyHomeAsp.MVC/Controllers/DiningRoomController.cs
+++ HappyHomeAsp.MVC/Controllers/DiningRoomController.cs
@@ -9,14 +9,27 @@ using System.Collections;
 using System.Web.Mvc;
 using System.Configuration;
 using MySql.Data.MySqlClient;
-
+using PagedList;
 namespace HappyHomeAsp.MVC.Controllers
 {
     public class DiningRoomController : Controller
     {
         // GET: DiningRoom
-        public ActionResult Index(int productTypeId)
+        public ActionResult Index(int productTypeId, int? page, int? pageSize)
         {
+            if (productTypeId == null)
+            {
+                productTypeId = -1;
+            }
+            if (page == null)
+            {
+                page = 1;
+            }
+            if(pageSize == null)
+            {
+                pageSize = 2;
+            }
+            ViewBag.PageSize = pageSize;
             ManageData manage = new ManageData();
             ArrayList products = new ArrayList();
             if (productTypeId >= 0)
@@ -27,6 +40,7 @@ namespace HappyHomeAsp.MVC.Controllers
             {
                 products = manage.selectAllProduct2();
             }
+
             List<ProductType> listNameType = manage.getNameProductTypes();
             ViewBag.listType = listNameType;
 
@@ -35,11 +49,15 @@ namespace HappyHomeAsp.MVC.Controllers
                 ViewBag.productTypeName = manage.getNameProductType(productTypeId);
             }
             else
-            {
+            { 
                 ViewBag.productTypeName = "Tất cả sản phẩm";
             }
-
-            return View(products);
+            List<Product> products2 = new List<Product>();
+            foreach(Product p in products)
+            {
+                products2.Add(p);
+            }
+            return View(products2.ToPagedList((int)page, (int)pageSize));
         }
 
     }
